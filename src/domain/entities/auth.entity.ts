235325de72dd19c9c3583c36de5ba8b30a638c81
Goomees <mateus.nicolau@devmagic.com.br>
2023-02-20 20:47:14 -0300

    feat(*): create entities basics

---
+++ src/domain/entities/auth.entity.ts
@@ -0,0 +1,14 @@
+import { iEntity } from "../contracts";
+import { ObjectManager } from "../utils";
+
+export class AuthEntity extends iEntity {
+
+    public readonly email : string
+    public readonly password : string
+    public readonly associeted_id : string 
+
+    constructor(auth : Partial<AuthEntity>){
+        super(auth);
+        ObjectManager.assing(this, auth)
+    }
+}
---
+++ src/domain/entities/user.entity.ts
@@ -0,0 +1,12 @@
+import { iEntity } from "../contracts";
+import { ObjectManager } from "../utils";
+
+export class UserEntity extends iEntity {
+
+    public readonly name : string
+
+    constructor(user : Partial<UserEntity>){
+        super(user)
+        ObjectManager.assing(this, user)
+    }
+}
