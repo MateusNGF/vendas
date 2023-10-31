3904865e3609825394fd924da7cd8cb5552d756e
Goomees <mateus.nicolau@devmagic.com.br>
2023-10-31 16:31:01 -0300

    add new test unit

---
+++ src/data/usecases/product/test/iArchiveOrUnarchiveProduct.test.ts
@@ -0,0 +1,41 @@
+import { MockProxy, mock } from "jest-mock-extended";
+import { iArchiveOrUnarchiveProductUsecase } from "src/domain/usecases/product";
+import { ArchiveOrUnarchiveProductData } from "../iArchiveOrUnarchiveProduct.data";
+import { iProductRepository } from "src/infra/database/contracts/repositorys/iProduct.repository";
+
+
+
+
+describe('CreateTokenAuthenticate', () => {
+    let sut: iArchiveOrUnarchiveProductUsecase;
+    let productRepository: MockProxy<iProductRepository>;
+
+    let fakeInputCredentials: iArchiveOrUnarchiveProductUsecase.Input;
+    let fakeOutput: iArchiveOrUnarchiveProductUsecase.Output;
+  
+    beforeAll(() => {
+        productRepository = mock();
+    });
+
+    beforeEach(() => {
+        sut = new ArchiveOrUnarchiveProductData(productRepository);
+    
+        fakeInputCredentials = {
+            action : 'archive',
+            product_id: '123123'
+        };
+        fakeOutput = true
+    });
+
+
+    it('Shound return null if not found authenticate by associeted_id', async () => {
+        // fakeInputCredentials.associeted_id = 'any_associeted';
+    
+        // authenticateRepository.findByAssocieted.mockResolvedValue(null);
+    
+        const result = await sut.exec(fakeInputCredentials);
+        expect(result).toEqual(null);
+    });
+
+
+})  
