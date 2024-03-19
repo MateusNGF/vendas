8e6c301c0c0f57c17b726409ce5d46c7b3e22e8e
Mateus Gomes <mateus.fernandes@pumpkintech.com.br>
2024-03-19 19:26:05 -0300

    fix: implement replset mongodb

--- src/infra/database/mongodb/scripts/index.mjs
+++ src/infra/database/mongodb/scripts/index.mjs
@@ -1,28 +1,41 @@
 import {MongoClient} from 'mongodb'
 (async ()=> {
-    await Promise.all([
-        createIndexes()
-    ])
+    let connection = null
+
+    const alert = setInterval(() => {
+        console.warn("Try connection with database at running pre-scripts ...")
+    }, 1000)
+
+    connection = await MongoClient.connect('mongodb://127.0.0.1:27017')
+    clearInterval(alert)
+
+    try{
+
+        await Promise.all([
+            createIndexes(connection)
+        ])
+
+    }catch(e){
+        console.log(e)
+        process.exit()
+    }finally{
+        await connection.close()
+    }
 })()
 
 
-async function createIndexes() {
-    let connection = null
-    try {
-        connection = await MongoClient.connect('mongodb://localhost:27017')
-        
-        await Promise.all([
-            CreateIndexForUsers(),
-            CreateIndexForProducts(),
-            CreateIndexForAuthenticates()
-        ])
-        
-    } catch (e) {
-        console.log(e)
-        process.exit()
-    } finally {
-        connection.close()
-    }
+/**
+ * Create all index of aplication
+ * @param {MongoClient} connection 
+ * @returns {void}
+ */
+async function createIndexes(connection) {
+
+    await Promise.all([
+        CreateIndexForUsers(),
+        CreateIndexForProducts(),
+        CreateIndexForAuthenticates()
+    ])
 
     function CreateIndexForUsers() {
         return connection.db('vendas').collection('users').createIndexes([
